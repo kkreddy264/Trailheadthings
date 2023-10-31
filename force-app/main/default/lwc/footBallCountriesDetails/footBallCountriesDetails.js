8dcc737b1ba727dcbe31d1757e20a9b0fef1a58d
kkreddy264 <kkreddy264@gmail.com>
2023-10-31 20:21:53 +0530

    changes to commit

--- force-app/main/default/lwc/footBallCountriesDetails/footBallCountriesDetails.js
+++ force-app/main/default/lwc/footBallCountriesDetails/footBallCountriesDetails.js
@@ -14,5 +14,5 @@ export default class footBallCountriesDetails extends LightningElement {
         }).catch(error=>{
             this.error = error.body;
         })
-    }
+    }  
 }
--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -2,7 +2,17 @@ import { LightningElement,api } from 'lwc';
 
 export default class Numerator extends LightningElement {
 
-  @api  counter = 0;
+  //@api  counter = 0;
+  _currentCount = 0;
+  priorCount = 0;
+  @api
+  get counter() {
+    return this._currentCount;
+  }
+  set counter(value) {
+    this.priorCount = this._currentCount;
+    this._currentCount = value;
+  }
   handleIncrement() {
     this.counter++;
   }
@@ -12,5 +22,9 @@ export default class Numerator extends LightningElement {
   handleMultiply(event) {
     const factor = event.detail;
     this.counter *= factor;
+  }
+  @api
+  maximizeCounter() {
+    this.counter += 1000000;
   }
 }
